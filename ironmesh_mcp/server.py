ab3d1f233a4fff448f26cf21e56f863a16b945f0
IronMesh <info@ironmesh.org>
2026-04-16 19:45:37 -0400

    chore: bump version to 0.8.3 (local, pre-release)

--- ironmesh_mcp/server.py
+++ ironmesh_mcp/server.py
@@ -66,7 +66,7 @@ log = logging.getLogger("ironmesh.mcp")
 # --------------------------------------------------------------------------
 
 PROTOCOL_VERSION = "2024-11-05"
-SERVER_INFO = {"name": "ironmesh", "version": "0.8.2"}
+SERVER_INFO = {"name": "ironmesh", "version": "0.8.3"}
 
 
 def _read_frame(stream) -> Optional[dict]:
